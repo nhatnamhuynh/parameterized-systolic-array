721a2226a6d400b747803161c3087f6e8699af18
pmhuy2302 <phamhuya307@gmail.com>
2026-09-12 21:49:58 +0700

    fix: update pe.v

--- RTL/pe.v
+++ RTL/pe.v
@@ -5,22 +5,22 @@ module pe #(
     input wire clk, rst, clr,
     input wire [DATA_WIDTH - 1: 0] row_in, col_in,
     output reg [DATA_WIDTH - 1: 0] row_out, col_out,
-    output reg [ACC_WIDTH - 1: 0] c_out
+    output reg [ACC_WIDTH - 1: 0] acc_out
 );
     
     always @(posedge clk or posedge rst) begin
         if (rst) begin
             row_out <= {DATA_WIDTH {1'b0}};
             col_out <= {DATA_WIDTH {1'b0}};
-            c_out <= {ACC_WIDTH{1'b0}};
+            acc_out <= {ACC_WIDTH{1'b0}};
         end else begin
             row_out <= row_in;
             col_out <= col_in;
 
             if (clr) begin
-                c_out <= row_in * col_in;
+                acc_out <= row_in * col_in;
             end else begin
-                c_out <= c_out + row_in * col_in;
+                acc_out <= acc_out + row_in * col_in;
             end
         end
     end
--- Testbench/pe_tb.v
+++ Testbench/pe_tb.v
@@ -3,8 +3,8 @@ module pe_tb;
     parameter ACC_WIDTH = 2 * DATA_WIDTH + 1;
 
     reg clk, rst, clr;
-    reg [DATA_WIDTH - 1: 0] a_in, b_in;
-    wire [DATA_WIDTH - 1: 0] a_out, b_out;
+    reg [DATA_WIDTH - 1: 0] row_in, col_in;
+    wire [DATA_WIDTH - 1: 0] row_out, col_out;
     wire [ACC_WIDTH - 1: 0] c_out;
     
 endmodule
