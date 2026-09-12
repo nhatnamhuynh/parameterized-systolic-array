bbecc117b4827faa7cfc95a5c624d7016858f4e0
pmhuy2302 <phamhuya307@gmail.com>
2026-09-12 21:47:53 +0700

    fix: update pe.v

--- RTL/pe.v
+++ RTL/pe.v
@@ -3,24 +3,24 @@ module pe #(
     parameter ACC_WIDTH = 2 * DATA_WIDTH + 1
 ) (
     input wire clk, rst, clr,
-    input wire [DATA_WIDTH - 1: 0] a_in, b_in,
-    output reg [DATA_WIDTH - 1: 0] a_out, b_out,
-    output reg [ACC_WIDTH: 0] c_out
+    input wire [DATA_WIDTH - 1: 0] row_in, col_in,
+    output reg [DATA_WIDTH - 1: 0] row_out, col_out,
+    output reg [ACC_WIDTH - 1: 0] c_out
 );
     
     always @(posedge clk or posedge rst) begin
         if (rst) begin
-            a_out <= {DATA_WIDTH {1'b0}};
-            b_out <= {DATA_WIDTH {1'b0}};
+            row_out <= {DATA_WIDTH {1'b0}};
+            col_out <= {DATA_WIDTH {1'b0}};
             c_out <= {ACC_WIDTH{1'b0}};
         end else begin
-            a_out <= a_in;
-            b_out <= b_in;
+            row_out <= row_in;
+            col_out <= col_in;
 
             if (clr) begin
-                c_out <= a_in * b_in;
+                c_out <= row_in * col_in;
             end else begin
-                c_out <= c_out + a_in * b_in;
+                c_out <= c_out + row_in * col_in;
             end
         end
     end
