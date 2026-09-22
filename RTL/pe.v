9a43966a2324ceba64edf929179595fd46745b44
pmhuy2302 <phamhuya307@gmail.com>
2026-09-22 21:15:27 +0700

    fix: update pe.v

--- RTL/pe.v
+++ RTL/pe.v
@@ -1,35 +1,59 @@
 module pe #(
+    parameter N = 2,
     parameter DATA_WIDTH = 8,
-    parameter ACC_WIDTH = 2 * DATA_WIDTH + 1
+    parameter ACC_WIDTH = 2 * DATA_WIDTH + $clog2(N)
 ) (
-    input wire clk,
-    input wire rst_in_row, rst_in_col, 
-    input wire clr_in_row, clr_in_col, 
-    input wire en_in_row, en_in_col,
-    input wire [ACC_WIDTH - 1: 0] result_in,
+    input wire clk, rst, clr,
+    input wire en_row_in, en_col_in,
+               ready_col_in, 
     input wire [DATA_WIDTH - 1: 0] row_in, col_in,
-    output reg [DATA_WIDTH - 1: 0] row_out, col_out,
-    output reg [ACC_WIDTH - 1: 0] result_out,
-    output reg rst_out_row, rst_out_col, 
-    output reg clr_out_row, clr_out_col, 
-    output reg en_out_row, en_out_col
-);
-    
-//    always @(posedge clk, posedge rst) begin
-//        if (rst) begin
-//            row_out <= {DATA_WIDTH {1'b0}};
-//            col_out <= {DATA_WIDTH {1'b0}};
-//            acc_out <= {ACC_WIDTH{1'b0}};
-//        end else begin
-//            row_out <= row_in;
-//            col_out <= col_in;
+    input wire [ACC_WIDTH - 1: 0] result_in,
 
-//            if (clr) begin
-//                acc_out <= $signed(row_in) * $signed(col_in);
-//            end else begin
-//                acc_out <= $signed(acc_out) + $signed(row_in) * $signed(col_in);
-//            end
-//        end
-//    end
+    output reg en_row_out, en_col_out,
+               ready_col_out,
+    output reg [DATA_WIDTH - 1: 0] row_out, col_out,
+    output reg [ACC_WIDTH - 1: 0] result_out
+);
+    reg [ACC_WIDTH - 1:0] pe_acc;
+
+    wire main_en;
+    assign main_en = (en_row_in && en_col_in)? 1'b1:1'b0;
+
+    
+    always @(posedge clk, posedge rst) begin
+        if (rst) begin
+            en_row_out <= 1'b0;
+            en_col_out <= 1'b0;
+            row_out <= {DATA_WIDTH {1'b0}};
+            col_out <= {DATA_WIDTH {1'b0}};
+            result_out <= {ACC_WIDTH{1'b0}};
+            ready_col_out <= 1'b0;
+            pe_acc <= {ACC_WIDTH{1'b0}};
+        end else begin
+            en_row_out <= en_row_in;
+            en_col_out <= en_col_in;
+
+            row_out <= row_in;
+            col_out <= col_in;
+            ready_col_out <= ready_col_in;
+
+            if (main_en) begin
+                pe_acc <= $signed(pe_acc) + $signed(row_in) * $signed(col_in);
+            end else begin
+                pe_acc <= pe_acc;
+            end
+
+            if (clr) begin
+            pe_acc <= {ACC_WIDTH{1'b0}};
+            end
+
+            if (ready_col_in) begin
+                result_out <= pe_acc;
+            end else begin
+                result_out <= result_in;
+            end
+        end
+
+    end
     
 endmodule
--- Testbench/pe_tb.v
+++ Testbench/pe_tb.v
@@ -1,19 +1,29 @@
 `timescale 1ns / 1ps
 
 module pe_tb;
+    parameter N = 2;
     parameter DATA_WIDTH = 8;
-    parameter ACC_WIDTH = 2 * DATA_WIDTH + 1;
+    parameter ACC_WIDTH = 2 * DATA_WIDTH + $clog2(N);
 
     reg clk, rst, clr;
     reg [DATA_WIDTH - 1: 0] row_in, col_in;
+    reg [ACC_WIDTH - 1: 0] result_in;
+    reg en_row_in, en_col_in, ready_col_in;
+
     wire [DATA_WIDTH - 1: 0] row_out, col_out;
-    wire [ACC_WIDTH - 1: 0] acc_out;
+    wire [ACC_WIDTH - 1: 0] pe_acc;
+    wire en_row_out, en_col_out, ready_col_out;
+    wire [ACC_WIDTH - 1: 0] result_out;
 
     pe #(.DATA_WIDTH(DATA_WIDTH)) uut (
         .clk(clk), .rst(rst), .clr(clr),
         .row_in(row_in), .col_in(col_in),
         .row_out(row_out), .col_out(col_out),
-        .acc_out(acc_out)
+        .pe_acc(pe_acc), 
+        .result_in(result_in), .result_out(result_out),
+        .en_row_in(en_row_in), .en_col_in(en_col_in),
+        .en_row_out(en_row_out), .en_col_out(en_col_out),
+        .ready_col_in(ready_col_in), .ready_col_out(ready_col_out)
     );
 
     initial begin
@@ -23,24 +33,24 @@ module pe_tb;
 
     task print_results;
         input [63:0] test;
-        input expected_rst, expected_clr;
+        input expected_rst, expected_clr, expected_ready_col_in, expected_main_en;
         input [DATA_WIDTH - 1: 0] expected_row_in, expected_col_in,
                                   expected_row_out, expected_col_out;
-        input [ACC_WIDTH - 1: 0] expected_acc_out;
+        input [ACC_WIDTH - 1: 0] expected_pe_acc, expected_result_out;
 
         begin 
             $display ("");
             $display ("--- %s ---", test);
             $display ("--- EXPECTED VALUE ---");
-            $display ("rst = %b \t clr = %b \t row_in = %3d \t col_in = %3d \t row_out = %3d \t col_out = %3d \t acc_out = %6d",
-                      expected_rst, expected_clr, $signed(expected_row_in), $signed(expected_col_in),
-                      $signed(expected_row_out), $signed(expected_col_out), $signed(expected_acc_out));
+            $display ("rst = %b \t clr = %b \t ready_col_in = %b \t row_in = %3d \t col_in = %3d \t row_out = %3d \t col_out = %3d \t pe_acc = %6d",
+                      expected_rst, expected_clr, expected_ready_col_in, $signed(expected_row_in), $signed(expected_col_in),
+                      $signed(expected_row_out), $signed(expected_col_out), $signed(expected_pe_acc));
             $display ("--- RECEIVED VALUE ---");
-            $display ("rst = %b \t clr = %b \t row_in = %3d \t col_in = %3d \t row_out = %3d \t col_out = %3d \t acc_out = %6d",
-                      rst, clr, $signed(row_in), $signed(col_in), $signed(row_out), $signed(col_out), $signed(acc_out));
+            $display ("rst = %b \t clr = %b \t ready_col_in = %b \t row_in = %3d \t col_in = %3d \t row_out = %3d \t col_out = %3d \t pe_acc = %6d",
+                      rst, clr, ready_col_in, $signed(row_in), $signed(col_in), $signed(row_out), $signed(col_out), $signed(pe_acc));
             
-            if (rst !== expected_rst || clr !== expected_clr || row_in !== expected_row_in || col_in !== expected_col_in ||
-                row_out !== expected_row_out || col_out !== expected_col_out || acc_out !== expected_acc_out) begin
+            if (rst !== expected_rst || clr !== expected_clr || ready_col_in !== expected_ready_col_in || row_in !== expected_row_in || col_in !== expected_col_in ||
+                row_out !== expected_row_out || col_out !== expected_col_out || pe_acc !== expected_pe_acc) begin
                 $display ("--- RESULT: FAILED ---");
             end else begin
                 $display ("--- RESULT: PASSED ---");
