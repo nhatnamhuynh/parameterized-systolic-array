ebbdbc63db40fefdb747c41dd7a8a46a5fc1b3cb
pmhuy2302 <phamhuya307@gmail.com>
2026-09-25 13:43:31 +0700

    fix: update pe.v have 2 register for addition and multiplication

--- RTL/pe.v
+++ RTL/pe.v
@@ -5,15 +5,15 @@ module pe #(
 ) (
     input wire clk, rst, clr,
     input wire en_row_in, en_col_in, ready, 
-    input wire [DATA_WIDTH - 1: 0] row_in, col_in,
-    input wire [ACC_WIDTH - 1: 0] result_in,
+    input wire signed [DATA_WIDTH - 1: 0] row_in, col_in,
+    input wire signed [ACC_WIDTH - 1: 0] result_in,
 
     output reg en_row_out, en_col_out,
-    output reg [DATA_WIDTH - 1: 0] row_out, col_out,
-    output reg [ACC_WIDTH - 1: 0] result_out
+    output reg signed [DATA_WIDTH - 1: 0] row_out, col_out,
+    output reg signed [ACC_WIDTH - 1: 0] result_out
 );
-    reg [ACC_WIDTH - 1:0] pe_acc;
-    reg [ACC_WIDTH - 1:0] mul_reg;
+    reg signed [ACC_WIDTH - 1:0] pe_acc;
+    reg signed [ACC_WIDTH - 1:0] mul_reg;
     reg mul_ready;
 
     wire main_en;
@@ -31,28 +31,23 @@ module pe #(
             mul_reg <= {ACC_WIDTH{1'b0}};
             mul_ready <= 1'b0;
         end else begin
+        
+            en_row_out <= en_row_in;
+            en_col_out <= en_col_in;
+            row_out <= row_in;
+            col_out <= col_in;
             
             if (clr) begin
                 pe_acc <= {ACC_WIDTH{1'b0}};
             end else if (mul_ready) begin
-                pe_acc <= $signed(pe_acc) + $signed(mul_reg);
+                pe_acc <= pe_acc + mul_reg;
             end
 
             if (main_en) begin
-                mul_reg <= $signed(row_in) * $signed(col_in);
+                mul_reg <= row_in * col_in;
                 mul_ready <= 1'b1;
-
-                en_row_out <= en_row_in;
-                en_col_out <= en_col_in;
-                row_out <= row_in;
-                col_out <= col_in;
-                
             end else begin
                 mul_ready <= 1'b0;
-                en_row_out <= 1'b0;
-                en_col_out <= 1'b0;
-                row_out <= {DATA_WIDTH {1'b0}};
-                col_out <= {DATA_WIDTH {1'b0}};
             end
 
             if (ready) begin
